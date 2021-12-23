245f3cf97b06d0280843ae6ec500c1435ff76def
danijela <dacafaca@gmail.com>
2021-12-23 02:08:28 +0100

    disable cors

--- fastfoods/src/main/java/pmf/gis/fastfoods/security/DisableSecurityConfig.java
+++ fastfoods/src/main/java/pmf/gis/fastfoods/security/DisableSecurityConfig.java
@@ -1,6 +1,5 @@
 package pmf.gis.fastfoods.security;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,7 +14,7 @@ public class DisableSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // @formatter:off
-        http
+        http.cors().and()
             .authorizeRequests()
                 .anyRequest().permitAll()
                 .and()
